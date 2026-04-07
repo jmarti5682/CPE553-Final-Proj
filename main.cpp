29eaea7c06d724fb5e17c683217bb7633ae1ab5a
Jose Martinez-Ponce <101851045+jmarti5682@users.noreply.github.com>
2026-04-06 21:15:43 -0400

    Update main.cpp
    - Added in visualize function to test

--- main.cpp
+++ main.cpp
@@ -2,19 +2,21 @@
 #include "libraries/oscillators/rch_oscillators.h"
 #include "ProcessAudio.h"
 #include "Tremolo.h"
+#include "Visualizer.h"
 using namespace std;
 
 int main()
 {
     ProcessAudio test("testaudio_clean.wav");
 
-    Tremolo tremolo;
+    visualize(test, "Pre-Effects"); // Before effects
 
-    test.processFile(tremolo);
+    test.processFile("tremolo");
+
+    visualize(test, "Post-Effects"); // After effects
 
     test.writeFile("output.wav");
 
     float sr = test.getSampleRate();
-
     cout << sr;
 }
